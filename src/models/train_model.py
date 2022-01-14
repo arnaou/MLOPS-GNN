fb8a938f6c9d288512f1650cf5ac35d05d3bf3bf
aswinanilvarkey96 <78964815+aswinanilvarkey96@users.noreply.github.com>
2022-01-14 17:20:59 +0100

    fixed formatting

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,18 +1,24 @@
-import torch
 import argparse
 import logging
-from torch import nn, optim
-from src.models.model import GNNModel
-import torch.nn.functional as F
-import wandb
 from math import sqrt
-from torch.profiler import profile, record_function, ProfilerActivity, tensorboard_trace_handler
+
+import torch
+import torch.nn.functional as F
+from torch.profiler import (
+    ProfilerActivity,
+    profile,
+    record_function,
+    tensorboard_trace_handler,
+)
+
+import wandb
+from src.models.model import GNNModel
 
 log = logging.getLogger(__name__)
 model = GNNModel.model
 criterion = torch.nn.NLLLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=10 ** -2.5, weight_decay=10 ** -5)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def train(train_loader):
@@ -35,55 +41,60 @@ def test(test_loader):
     for data in test_loader:
         data = data.to(device)
         out = model(data.x, data.edge_index, data.edge_attr, data.batch)
-        mse.append(F.mse_loss(out, data.y, reduction='none').cpu())
+        mse.append(F.mse_loss(out, data.y, reduction="none").cpu())
     return float(torch.cat(mse, dim=0).mean().sqrt())
 
 
-def train_loop():    
+def train_loop():
 
-    with profile(schedule=torch.profiler.schedule(
-        wait=5, # during this phase profiler is not active
-        warmup=2, # during this phase profiler starts tracing, but the results are discarded
-        active=6, # during this phase profiler traces and records data
-        repeat=2),
-        activities=[ProfilerActivity.CPU], 
-        record_shapes=True, 
-        on_trace_ready=tensorboard_trace_handler('./log/train/')
-        ) as prof:
+    with profile(
+        schedule=torch.profiler.schedule(
+            wait=5,  # during this phase profiler is not active
+            warmup=2,  # during this phase profiler starts tracing, but the results are discarded
+            active=6,  # during this phase profiler traces and records data
+            repeat=2,
+        ),
+        activities=[ProfilerActivity.CPU],
+        record_shapes=True,
+        on_trace_ready=tensorboard_trace_handler("./log/train/"),
+    ) as prof:
         with record_function("model_inference"):
 
-            for epoch in range(1, epochs+1):
+            for epoch in range(1, epochs + 1):
                 train_rmse = train(train_loader)
                 val_rmse = test(val_loader)
-                
+
                 wandb.log({"train_rmse": train_rmse})
                 wandb.log({"val_rmse": val_rmse})
-                print(f'Epoch: {epoch:03d}, Train Loss: {train_rmse:.4f} Val Loss: {val_rmse:.4f} ')
+                print(
+                    f"Epoch: {epoch:03d}, Train Loss: {train_rmse:.4f} Val Loss: {val_rmse:.4f} "
+                )
                 prof.step()
 
-            torch.save(model.state_dict(),checkpoint) 
+            torch.save(model.state_dict(), checkpoint)
 
-if __name__ == "__main__":   
 
-    parser = argparse.ArgumentParser(description='Train Model')
-    parser.add_argument('--config', default = 'config_default.yaml')
-    
-    args = parser.parse_args() 
+if __name__ == "__main__":
 
-    config_path = 'src/configs/' + str(args.config)
-    wandb.init(project = 'MLOPS-GNN', config = config_path)
+    parser = argparse.ArgumentParser(description="Train Model")
+    parser.add_argument("--config", default="config_default.yaml")
+
+    args = parser.parse_args()
+
+    config_path = "src/configs/" + str(args.config)
+    wandb.init(project="MLOPS-GNN", config=config_path)
     config = wandb.config
     wandb.watch(model)
-    
-    epochs      = config.epochs
-    lr          = config.learning_rate
-    trainpath   = config.trainpath
-    valpath     = config.valpath
-    checkpoint  = config.checkpoint       
+
+    epochs = config.epochs
+    lr = config.learning_rate
+    trainpath = config.trainpath
+    valpath = config.valpath
+    checkpoint = config.checkpoint
 
     log.info("Training day and night")
-    train_loader    = torch.load(trainpath)
-    val_loader      = torch.load(valpath)
-    
-    train_loop()                       
+    train_loader = torch.load(trainpath)
+    val_loader = torch.load(valpath)
+
+    train_loop()
     wandb.finish()
