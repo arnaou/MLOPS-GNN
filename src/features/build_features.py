01221086a5c18647534b3fad4bf75f24ad9d23fb
arnaou <arnaou@kt.dtu.dk>
2022-01-19 14:57:56 +0100

    made feature function for inference

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -113,7 +113,7 @@ class Gen_Afp_Features(object):
         return data
 
 
-def process_my_smiles(smiles, pre_transform=None):
+def process_my_smiles(smiles):
     from rdkit import Chem
 
     x_map = {
@@ -211,14 +211,213 @@ def process_my_smiles(smiles, pre_transform=None):
 
     data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smiles=smiles)
 
-    if pre_transform is not None:
-        data = self.pre_transform(data)
+    # attentive fp featurization
 
     return data
 
 
+def process_smiles(smiles):
+    from rdkit import Chem
 
-import pdb
+    x_map = {
+        'atomic_num':
+            list(range(0, 119)),
+        'chirality': [
+            'CHI_UNSPECIFIED',
+            'CHI_TETRAHEDRAL_CW',
+            'CHI_TETRAHEDRAL_CCW',
+            'CHI_OTHER',
+        ],
+        'degree':
+            list(range(0, 11)),
+        'formal_charge':
+            list(range(-5, 7)),
+        'num_hs':
+            list(range(0, 9)),
+        'num_radical_electrons':
+            list(range(0, 5)),
+        'hybridization': [
+            'UNSPECIFIED',
+            'S',
+            'SP',
+            'SP2',
+            'SP3',
+            'SP3D',
+            'SP3D2',
+            'OTHER',
+        ],
+        'is_aromatic': [False, True],
+        'is_in_ring': [False, True],
+    }
 
-if __name__ == "__main__":
-    process_my_smiles("CCCCC")
+    e_map = {
+        'bond_type': [
+            'misc',
+            'SINGLE',
+            'DOUBLE',
+            'TRIPLE',
+            'AROMATIC',
+        ],
+        'stereo': [
+            'STEREONONE',
+            'STEREOZ',
+            'STEREOE',
+            'STEREOCIS',
+            'STEREOTRANS',
+            'STEREOANY',
+        ],
+        'is_conjugated': [False, True],
+    }
+
+    pdb.set_trace()
+    mol = Chem.MolFromSmiles(smiles)
+    assert mol != None, "the smiles provided could not be parsed bt RDKit"
+    # node featurization
+    xs = []
+    for atom in mol.GetAtoms():
+        x = []
+        x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
+        x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
+        x.append(x_map['degree'].index(atom.GetTotalDegree()))
+        x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
+        x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
+        x.append(x_map['num_radical_electrons'].index(
+            atom.GetNumRadicalElectrons()))
+        x.append(x_map['hybridization'].index(
+            str(atom.GetHybridization())))
+        x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
+        x.append(x_map['is_in_ring'].index(atom.IsInRing()))
+        xs.append(x)
+
+    x = torch.tensor(xs, dtype=torch.long).view(-1, 9)
+
+    edge_indices, edge_attrs = [], []
+    for bond in mol.GetBonds():
+        i = bond.GetBeginAtomIdx()
+        j = bond.GetEndAtomIdx()
+
+        e = []
+        e.append(e_map['bond_type'].index(str(bond.GetBondType())))
+        e.append(e_map['stereo'].index(str(bond.GetStereo())))
+        e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
+
+        edge_indices += [[i, j], [j, i]]
+        edge_attrs += [e, e]
+
+    edge_index = torch.tensor(edge_indices)
+    edge_index = edge_index.t().to(torch.long).view(2, -1)
+    edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
+
+    # Sort indices.
+    if edge_index.numel() > 0:
+        perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
+        edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
+
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smiles=smiles)
+
+    # afp featurize
+    symbols = [
+        "B",
+        "C",
+        "N",
+        "O",
+        "F",
+        "Si",
+        "P",
+        "S",
+        "Cl",
+        "As",
+        "Se",
+        "Br",
+        "Te",
+        "I",
+        "At",
+        "other",
+    ]
+
+    hybridizations = [
+        Chem.rdchem.HybridizationType.SP,
+        Chem.rdchem.HybridizationType.SP2,
+        Chem.rdchem.HybridizationType.SP3,
+        Chem.rdchem.HybridizationType.SP3D,
+        Chem.rdchem.HybridizationType.SP3D2,
+        "other",
+    ]
+
+    stereos = [
+        Chem.rdchem.BondStereo.STEREONONE,
+        Chem.rdchem.BondStereo.STEREOANY,
+        Chem.rdchem.BondStereo.STEREOZ,
+        Chem.rdchem.BondStereo.STEREOE,
+    ]
+    mol = Chem.MolFromSmiles(data.smiles)
+
+    xs = []
+    for atom in mol.GetAtoms():
+        symbol = [0.0] * len(symbols)
+        symbol[symbols.index(atom.GetSymbol())] = 1.0
+        degree = [0.0] * 6
+        degree[atom.GetDegree()] = 1.0
+        formal_charge = atom.GetFormalCharge()
+        radical_electrons = atom.GetNumRadicalElectrons()
+        hybridization = [0.0] * len(hybridizations)
+        hybridization[hybridizations.index(atom.GetHybridization())] = 1.0
+        aromaticity = 1.0 if atom.GetIsAromatic() else 0.0
+        hydrogens = [0.0] * 5
+        hydrogens[atom.GetTotalNumHs()] = 1.0
+        chirality = 1.0 if atom.HasProp("_ChiralityPossible") else 0.0
+        chirality_type = [0.0] * 2
+        if atom.HasProp("_CIPCode"):
+            chirality_type[["R", "S"].index(atom.GetProp("_CIPCode"))] = 1.0
+
+        x = torch.tensor(
+            symbol
+            + degree
+            + [formal_charge]
+            + [radical_electrons]
+            + hybridization
+            + [aromaticity]
+            + hydrogens
+            + [chirality]
+            + chirality_type
+        )
+        xs.append(x)
+
+    data.x = torch.stack(xs, dim=0)
+
+    edge_indices = []
+    edge_attrs = []
+    for bond in mol.GetBonds():
+        edge_indices += [[bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()]]
+        edge_indices += [[bond.GetEndAtomIdx(), bond.GetBeginAtomIdx()]]
+
+        bond_type = bond.GetBondType()
+        single = 1.0 if bond_type == Chem.rdchem.BondType.SINGLE else 0.0
+        double = 1.0 if bond_type == Chem.rdchem.BondType.DOUBLE else 0.0
+        triple = 1.0 if bond_type == Chem.rdchem.BondType.TRIPLE else 0.0
+        aromatic = 1.0 if bond_type == Chem.rdchem.BondType.AROMATIC else 0.0
+        conjugation = 1.0 if bond.GetIsConjugated() else 0.0
+        ring = 1.0 if bond.IsInRing() else 0.0
+        stereo = [0.0] * 4
+        stereo[stereos.index(bond.GetStereo())] = 1.0
+
+        edge_attr = torch.tensor(
+            [single, double, triple, aromatic, conjugation, ring] + stereo
+        )
+
+        edge_attrs += [edge_attr, edge_attr]
+
+    if len(edge_attrs) == 0:
+        data.edge_index = torch.zeros((2, 0), dtype=torch.long)
+        data.edge_attr = torch.zeros((0, 10), dtype=torch.float)
+    else:
+        data.edge_index = torch.tensor(edge_indices).t().contiguous()
+        data.edge_attr = torch.stack(edge_attrs, dim=0)
+
+    return data
+
+
+#import pdb
+
+#if __name__ == "__main__":
+#    process_smiles("CCCCC")
