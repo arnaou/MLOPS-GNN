562df541d442bd362b12025057cfb91b8fbe0a6d
arnaou <arnaou@kt.dtu.dk>
2022-01-19 14:37:34 +0100

    made feature function for inference

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -113,176 +113,112 @@ class Gen_Afp_Features(object):
         return data
 
 
-x_map = {
-    'atomic_num':
-        list(range(0, 119)),
-    'chirality': [
-        'CHI_UNSPECIFIED',
-        'CHI_TETRAHEDRAL_CW',
-        'CHI_TETRAHEDRAL_CCW',
-        'CHI_OTHER',
-    ],
-    'degree':
-        list(range(0, 11)),
-    'formal_charge':
-        list(range(-5, 7)),
-    'num_hs':
-        list(range(0, 9)),
-    'num_radical_electrons':
-        list(range(0, 5)),
-    'hybridization': [
-        'UNSPECIFIED',
-        'S',
-        'SP',
-        'SP2',
-        'SP3',
-        'SP3D',
-        'SP3D2',
-        'OTHER',
-    ],
-    'is_aromatic': [False, True],
-    'is_in_ring': [False, True],
-}
+def process_my_smiles(smiles, pre_transform=None):
+    from rdkit import Chem
 
-e_map = {
-    'bond_type': [
-        'misc',
-        'SINGLE',
-        'DOUBLE',
-        'TRIPLE',
-        'AROMATIC',
-    ],
-    'stereo': [
-        'STEREONONE',
-        'STEREOZ',
-        'STEREOE',
-        'STEREOCIS',
-        'STEREOTRANS',
-        'STEREOANY',
-    ],
-    'is_conjugated': [False, True],
-}
+    x_map = {
+        'atomic_num':
+            list(range(0, 119)),
+        'chirality': [
+            'CHI_UNSPECIFIED',
+            'CHI_TETRAHEDRAL_CW',
+            'CHI_TETRAHEDRAL_CCW',
+            'CHI_OTHER',
+        ],
+        'degree':
+            list(range(0, 11)),
+        'formal_charge':
+            list(range(-5, 7)),
+        'num_hs':
+            list(range(0, 9)),
+        'num_radical_electrons':
+            list(range(0, 5)),
+        'hybridization': [
+            'UNSPECIFIED',
+            'S',
+            'SP',
+            'SP2',
+            'SP3',
+            'SP3D',
+            'SP3D2',
+            'OTHER',
+        ],
+        'is_aromatic': [False, True],
+        'is_in_ring': [False, True],
+    }
+
+    e_map = {
+        'bond_type': [
+            'misc',
+            'SINGLE',
+            'DOUBLE',
+            'TRIPLE',
+            'AROMATIC',
+        ],
+        'stereo': [
+            'STEREONONE',
+            'STEREOZ',
+            'STEREOE',
+            'STEREOCIS',
+            'STEREOTRANS',
+            'STEREOANY',
+        ],
+        'is_conjugated': [False, True],
+    }
+    pdb.set_trace()
+    mol = Chem.MolFromSmiles(smiles)
+    assert mol != None, "the smiles provided could not be parsed bt RDKit"
+    # node featurization
+    xs = []
+    for atom in mol.GetAtoms():
+        x = []
+        x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
+        x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
+        x.append(x_map['degree'].index(atom.GetTotalDegree()))
+        x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
+        x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
+        x.append(x_map['num_radical_electrons'].index(
+            atom.GetNumRadicalElectrons()))
+        x.append(x_map['hybridization'].index(
+            str(atom.GetHybridization())))
+        x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
+        x.append(x_map['is_in_ring'].index(atom.IsInRing()))
+        xs.append(x)
+
+    x = torch.tensor(xs, dtype=torch.long).view(-1, 9)
+
+    edge_indices, edge_attrs = [], []
+    for bond in mol.GetBonds():
+        i = bond.GetBeginAtomIdx()
+        j = bond.GetEndAtomIdx()
+
+        e = []
+        e.append(e_map['bond_type'].index(str(bond.GetBondType())))
+        e.append(e_map['stereo'].index(str(bond.GetStereo())))
+        e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
+
+        edge_indices += [[i, j], [j, i]]
+        edge_attrs += [e, e]
+
+    edge_index = torch.tensor(edge_indices)
+    edge_index = edge_index.t().to(torch.long).view(2, -1)
+    edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
+
+    # Sort indices.
+    if edge_index.numel() > 0:
+        perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
+        edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
+
+    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, smiles=smiles)
+
+    if pre_transform is not None:
+        data = self.pre_transform(data)
+
+    return data
 
 
-class MoleculeNet_inference(InMemoryDataset):
-    r"""The moleculeNet_inference takes a SMILES notation for the molecule
-    and produces PYG data class. It is a modified version of the class
-    defined <https://pytorch-geometric.readthedocs.io/en/latest/_modules/torch_geometric/datasets/molecule_net.html#MoleculeNet>
 
-    Args:
-        smiles (string): SMILES notation of the molecule
-        pre_transform (callable, optional): A function/transform that takes in
-            an :obj:`torch_geometric.data.Data` object and returns a
-            transformed version. The data object will be transformed before
-            being saved to disk. (default: :obj:`None`)
-    """
+import pdb
 
-
-    def __init__(self, root, name, transform=None, pre_transform=None,
-                 pre_filter=None):
-        self.name = name.lower()
-        assert self.name in self.names.keys()
-        super().__init__(root, transform, pre_transform, pre_filter)
-        self.data, self.slices = torch.load(self.processed_paths[0])
-
-    @property
-    def raw_dir(self):
-        return osp.join(self.root, self.name, 'raw')
-
-    @property
-    def processed_dir(self):
-        return osp.join(self.root, self.name, 'processed')
-
-    @property
-    def raw_file_names(self):
-        return f'{self.names[self.name][2]}.csv'
-
-    @property
-    def processed_file_names(self):
-        return 'data.pt'
-
-    def download(self):
-        url = self.url.format(self.names[self.name][1])
-        path = download_url(url, self.raw_dir)
-        if self.names[self.name][1][-2:] == 'gz':
-            extract_gz(path, self.raw_dir)
-            os.unlink(path)
-
-    def process(self):
-        from rdkit import Chem
-
-        with open(self.raw_paths[0], 'r') as f:
-            dataset = f.read().split('\n')[1:-1]
-            dataset = [x for x in dataset if len(x) > 0]  # Filter empty lines.
-
-        data_list = []
-        for line in dataset:
-            line = re.sub(r'\".*\"', '', line)  # Replace ".*" strings.
-            line = line.split(',')
-
-            smiles = line[self.names[self.name][3]]
-            ys = line[self.names[self.name][4]]
-            ys = ys if isinstance(ys, list) else [ys]
-
-            ys = [float(y) if len(y) > 0 else float('NaN') for y in ys]
-            y = torch.tensor(ys, dtype=torch.float).view(1, -1)
-
-            mol = Chem.MolFromSmiles(smiles)
-            if mol is None:
-                continue
-
-            xs = []
-            for atom in mol.GetAtoms():
-                x = []
-                x.append(x_map['atomic_num'].index(atom.GetAtomicNum()))
-                x.append(x_map['chirality'].index(str(atom.GetChiralTag())))
-                x.append(x_map['degree'].index(atom.GetTotalDegree()))
-                x.append(x_map['formal_charge'].index(atom.GetFormalCharge()))
-                x.append(x_map['num_hs'].index(atom.GetTotalNumHs()))
-                x.append(x_map['num_radical_electrons'].index(
-                    atom.GetNumRadicalElectrons()))
-                x.append(x_map['hybridization'].index(
-                    str(atom.GetHybridization())))
-                x.append(x_map['is_aromatic'].index(atom.GetIsAromatic()))
-                x.append(x_map['is_in_ring'].index(atom.IsInRing()))
-                xs.append(x)
-
-            x = torch.tensor(xs, dtype=torch.long).view(-1, 9)
-
-            edge_indices, edge_attrs = [], []
-            for bond in mol.GetBonds():
-                i = bond.GetBeginAtomIdx()
-                j = bond.GetEndAtomIdx()
-
-                e = []
-                e.append(e_map['bond_type'].index(str(bond.GetBondType())))
-                e.append(e_map['stereo'].index(str(bond.GetStereo())))
-                e.append(e_map['is_conjugated'].index(bond.GetIsConjugated()))
-
-                edge_indices += [[i, j], [j, i]]
-                edge_attrs += [e, e]
-
-            edge_index = torch.tensor(edge_indices)
-            edge_index = edge_index.t().to(torch.long).view(2, -1)
-            edge_attr = torch.tensor(edge_attrs, dtype=torch.long).view(-1, 3)
-
-            # Sort indices.
-            if edge_index.numel() > 0:
-                perm = (edge_index[0] * x.size(0) + edge_index[1]).argsort()
-                edge_index, edge_attr = edge_index[:, perm], edge_attr[perm]
-
-            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr, y=y,
-                        smiles=smiles)
-
-            if self.pre_filter is not None and not self.pre_filter(data):
-                continue
-
-            if self.pre_transform is not None:
-                data = self.pre_transform(data)
-
-            data_list.append(data)
-
-        torch.save(self.collate(data_list), self.processed_paths[0])
-
-    def __repr__(self) -> str:
-        return f'{self.names[self.name][0]}({len(self)})'
+if __name__ == "__main__":
+    process_my_smiles("CCCCC")
